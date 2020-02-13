8ba4bedf59527342b26849756b9f4bd7f6ce8aaf
majid <majid.paktinat@gmail.com>
2020-02-13 13:05:42 -0500

    api call code is done and results fetched sucessfully

--- Assets/script.js
+++ Assets/script.js
@@ -3,11 +3,12 @@ let nutritionArr = [];
 let analyzedInstructionsArr = [];
 
 let imageAdd;
+let mealImage
 
 let settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://api.spoonacular.com/recipes/complexSearch?minFat=1&maxFat=50&number=2&type=salad&addRecipeInformation=true&apiKey=7be734db65ca420487a133024e247177",
+	"url": "https://api.spoonacular.com/recipes/complexSearch?minFat=1&maxFat=50&minCarbs=5&maxCarbs=100&number=5&type=salad&addRecipeInformation=true&apiKey=54f37f7c8cf54dabb996147b296b7f34",
 	"method": "GET"
 	}
 
@@ -21,35 +22,43 @@ function getResponse( response ){
     console.log(`<.Then> callback <${response}>`);console.log(response);
     let resultsCounter = 0;
     while (resultsCounter<response.results.length){
+
         console.log(`Id is : ${response.results[resultsCounter].id}`);
         console.log(`Title is : ${response.results[resultsCounter].title}`);
         console.log(`Dish Types : ${response.results[resultsCounter].dishTypes}`);
-        nutritionArr = response.results[resultsCounter].nutrition; 
-            console.log(`Nutrition Title : ${nutritionArr[0].title}`); 
-            console.log(`Nutrition Amount : ${nutritionArr[0].amount}`); 
-            console.log(`Nutrition Unit : ${nutritionArr[0].unit}`); 
-        analyzedInstructionsArr = response.results[resultsCounter].analyzedInstructions; 
-            console.log(`analyzedInstructions Name : ${analyzedInstructionsArr[0].name}`); 
-            let analyzedInstructionsCounter=0;
-            console.log("AAAAAAAAAAAA");
-            console.log(analyzedInstructionsArr);
-            console.log(analyzedInstructionsArr.length);
-            
-            let stepsCounter = 0;
-            while (analyzedInstructionsCounter<analyzedInstructionsArr.length){
-                while(analyzedInstructionsCounter<analyzedInstructionsArr[analyzedInstructionsCounter].steps.length){
-                    console.log(`analyzedInstructions Steps(number) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].number}`);
-                    console.log(`analyzedInstructions Steps(step) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].step}`);
-                    stepsCounter = stepsCounter + 1;
-                }
-                analyzedInstructionsCounter = analyzedInstructionsCounter + 1;
-                stepsCounter = 0;
-            }
 
-           
-            
-        imageAdd= response.results[0].image; console.log(`Image is : ${imageAdd}`); 
-        document.querySelector("#imgTest").setAttribute("src", imageAdd);
+        nutritionArr = response.results[resultsCounter].nutrition; 
+        let nutritionCounter = 0;
+        while(nutritionCounter<nutritionArr.length){
+            console.log(`Nutrition Title : ${nutritionArr[nutritionCounter].title}`); 
+            console.log(`Nutrition Amount : ${nutritionArr[nutritionCounter].amount}`); 
+            console.log(`Nutrition Unit : ${nutritionArr[nutritionCounter].unit}`); 
+            console.log("--nutrition--");
+            nutritionCounter = nutritionCounter + 1;
+        }
+
+
+        analyzedInstructionsArr = response.results[resultsCounter].analyzedInstructions; 
+        let analyzedInstructionsCounter=0;
+        let stepsCounter = 0;
+        while (analyzedInstructionsCounter<analyzedInstructionsArr.length){
+            while(stepsCounter<analyzedInstructionsArr[analyzedInstructionsCounter].steps.length){
+                console.log(`analyzedInstructions Name : ${analyzedInstructionsArr[analyzedInstructionsCounter].name}`); 
+                console.log(`analyzedInstructions Steps(number) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].number}`);
+                console.log(`analyzedInstructions Steps(step) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].step}`);
+                stepsCounter = stepsCounter + 1;
+            }
+            console.log("---------------------------------------");
+            analyzedInstructionsCounter = analyzedInstructionsCounter + 1;
+            stepsCounter = 0;
+        }
+
+        
+        imageAdd = response.results[resultsCounter].image; 
+        mealImage = $('<img />');
+        mealImage.attr("src", imageAdd);
+        $('#meal-image-view').append(mealImage);
+        
         resultsCounter = resultsCounter + 1;
     }
 
